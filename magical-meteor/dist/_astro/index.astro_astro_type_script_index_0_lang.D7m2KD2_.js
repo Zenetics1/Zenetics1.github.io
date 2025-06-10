9842902bdb4cf07dad037957f5e3fa8a4bd3615d
Zenetics1 <williamzanderkim@gmail.com>
2025-06-10 00:01:41 -0400

    reset

--- magical-meteor/dist/_astro/index.astro_astro_type_script_index_0_lang.D7m2KD2_.js
+++
@@ -1,1 +0,0 @@
-import"https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js";
--- magical-meteor/public/js/main.js
+++
@@ -1,7 +0,0 @@
-var Typed = new Typed('.text', {
-    strings:["a Dreamer", "an Innovator", "a Team Player"],
-    typeSpeed:150,
-    backSpeed:150,
-    backDelay:1000,
-    loop:true
-});
