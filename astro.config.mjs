3d834299def740f6446ae43a9d5380f15365f8cf
Zenetics1 <spitfirexigames@gmail.com>
2025-06-28 14:12:04 -0400

    Organize Code+Reverse Last changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'wizk.ca',
+    site: 'Zenetics1.github.io',
     vite: {
         plugins: [tailwindcss()],
     }
