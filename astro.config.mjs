64c454f6c7ec60432973aba32c8a3a95ef3771ee
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2025-06-28 14:28:13 -0400

    URL fix #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'WiZK1.github.io',
+    site: 'https://wizk.ca',
     vite: {
         plugins: [tailwindcss()],
     }
